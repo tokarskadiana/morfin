5f52f30013f6e4fba7d71fe45658965ba1756812
Diana Tokarska <tokarskadiana@gmail.com>
2017-10-28 17:16:30 +0200

    Add firm model

---
+++ MorFin/Models/Firm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MorFin.Models
+{
+    public class Firm
+    {
+        [Required, MaxLength(255)]
+        public string Name;
+        [Required, MinLength(10), MaxLength(10)]
+        public int NIP;
+        public string Email;
+        public int PhoneNumber;
+        public IList<string> AuctionUrls;
+    }
+}
