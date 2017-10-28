7a57926c05ce2d9f527c9a18af14a8189b259453
Diana Tokarska <tokarskadiana@gmail.com>
2017-10-28 16:16:24 +0200

    Add project files.

---
+++ MorFin/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MorFin.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
